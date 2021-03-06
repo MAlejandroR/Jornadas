71818e60438d8c05a4054fb0a2704f52efe8dbf3
MAlejandroR <manuelromeromiguel@gmail.com>
2021-03-07 00:55:30 +0100

    realizando el index, sin terminar (falta cortar video y realizar imagenes)

---
+++ resources/views/components/layout/main/video.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <video src="{{assert('storage/videos/presentacion.mp4'}}" width=320  height=240 controls poster="asset{{'public/images/logo_calidad.png'}}">>
+    <video src="ejemplo.mp4"
+        Lo sentimos. Este vídeo no puede ser reproducido en tu navegador.<br>
+        La versión descargable está disponible en <a href="URL">Enlace</a>.
+    </video>
+    </video>
+</div>
